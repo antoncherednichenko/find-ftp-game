d7aa4f8439bda13121d8fead8303ae75a1b5c669
tonycherednichenko <tony.cherednichenko95@yandex.ru>
2022-05-01 17:31:55 +0300

    fixed select menu

--- src/components/Filter/Filter.vue
+++ src/components/Filter/Filter.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="{ sticky: !isBurger, 'max-w-300px': isBurger }" class=" w-full px-5 z-10 top-0 bg-primary-grey">
-        <div :class="{ hidden: !isBurger, flex: isBurger, 'flex-wrap': isBurger, 'max-w-300px': isBurger }" class="lg:flex  overflow-x-auto w-full max-w-screen-md justify-between items-center f-p">
+        <div :class="{ hidden: !isBurger, flex: isBurger, 'flex-wrap': isBurger, 'max-w-300px': isBurger }" class="lg:flex w-full max-w-screen-md justify-between items-center f-p">
             <WhiteLink 
                 @click.native="toggleSort(e.id)" 
                 v-for="e in filterList" 
