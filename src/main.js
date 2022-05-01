6a7a77fbd7e5fe80724ed59f4c887a7308577b9c
tonycherednichenko <tony.cherednichenko95@yandex.ru>
2022-05-01 18:24:10 +0300

    firebase deploy

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import VueCarousel from 'vue-carousel'
 import './assets/tailwind.css'
+import { initializeApp } from "firebase/app";
 
 Vue.config.productionTip = false
 
@@ -17,3 +18,14 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDp48OmOzFqNGKrSdgD72t2Yup-fqPKTbE',
+  authDomain: 'ftp-games-733b7.firebaseapp.com',
+  projectId: 'ftp-games-733b7',
+  storageBucket: 'ftp-games-733b7.appspot.com',
+  messagingSenderId: '569793341090',
+  appId: '1:569793341090:web:5085b2f239953257b5d9fb'
+}
+
+const app = initializeApp(firebaseConfig)
